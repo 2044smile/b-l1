52719a6d08a89c0b5aca7cf58024740e04fcbdab
2044smile <2044smile@naver.com>
2022-05-20 19:58:09 +0900

    feat: b-l2 desc Dockerfile, script.sh, container

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*g*gy&4sf^8y@7mp1zc#-frp4tw5=mtgpba)(%l0gubl2@tha_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
---
+++ mysite/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "migrate"
+python manage.py migrate
+
+echo "runserver"
+python manage.py runserver 0.0.0.0:8000
