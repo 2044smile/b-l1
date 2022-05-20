85cad768ae075542e457c22e7fd9ffff59ed538f
2044smile <2044smile@naver.com>
2022-05-21 01:01:10 +0900

    feat: b-m2 vietnam app 하나 더 생성

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('vietnam/', include('vietnam.urls')),
+
+    path('api-auth', include('rest_framework.urls')),
 
     path('admin/', admin.site.urls),
 ]
