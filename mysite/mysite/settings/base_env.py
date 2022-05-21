910dea14b883426a881fbfa340db6993aaa3e8cc
2044smile <2044smile@naver.com>
2022-05-21 12:38:24 +0900

    feat: b-m2 Django setting 변경, base_env, dev_env, prod_env 생성완료 b-h1에서 DB를 연결하니 그 때 사용해보자 / secrets.json 생성 gitignore 추가완료 / DJANGO_SEETINGS_MODULE 설정 완료 base_env

--- mysite/mysite/settings/base_env.py
+++ mysite/mysite/settings/base_env.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +21,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*g*gy&4sf^8y@7mp1zc#-frp4tw5=mtgpba)(%l0gubl2@tha_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
+APPEND_SLASH = False
+# False 를 붙여주는게 혹시 모를 실수에도 좋아보이지만, 난 항상 슬래시를 붙여준다. (습관)
+
+secret_file = os.path.join(BASE_DIR, '../secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
 
-# Application definition
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 INSTALLED_APPS = [
     'polls',
@@ -81,7 +97,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
